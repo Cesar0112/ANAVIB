eaa05e623b339ce87dc07549786c697dd297795d
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-06-15 11:25:56 -0400

    Agregue el proyecto

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
+  FMX.ExtCtrls;
 
 type
   TForm1 = class(TForm)
+    PlotGrid1: TPlotGrid;
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
   private
     { Private declarations }
   public
