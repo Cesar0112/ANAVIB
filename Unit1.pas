6d2b6439dde5ba94a2044fad31c87a031fb56d03
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-06-26 22:01:34 -0400

    Cambio de disposición de lugares
    Principalmente este va a ser la primera version del software

--- Unit1.pas
+++ Unit1.pas
@@ -6,7 +6,8 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
-  FMX.ExtCtrls, FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.ExtCtrls, FMX.Controls.Presentation, FMX.StdCtrls, FMXTee.Engine,
+  FMXTee.Procs, FMXTee.Chart, FMX.Controls3D, FMXTee.Chart3D, FMXTee.Series;
 
 type
   TForm1 = class(TForm)
@@ -14,9 +15,35 @@ type
     opcionRuta: TMenuItem;
     opcionSalir: TMenuItem;
     opcionRutaVer: TMenuItem;
-    opcionRutaCrear: TMenuItem;
-    Panel1: TPanel;
-    procedure MenuItem2Click(Sender: TObject);
+    Configuración: TMenuItem;
+    opcionAnalisisTendencia: TMenuItem;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    btnMostrar: TButton;
+    btnCongelar: TButton;
+    graficoSenial: TChart;
+    Series1: TFastLineSeries;
+    lblPicoMax: TLabel;
+    lblPicoMin: TLabel;
+    lblRMS: TLabel;
+    label2: TLabel;
+    valoresGlobales: TGroupBox;
+    lblMuestraValorRMS: TLabel;
+    lblMuestraValorPicoMaximo: TLabel;
+    lblMuestraValorPicoMinimo: TLabel;
+    lblMuestraValorDePicoPico: TLabel;
+    btnRegistrar: TButton;
+    comandosGrafico: TGroupBox;
+    graficoEspectro: TChart;
+    FastLineSeries1: TFastLineSeries;
+    panelPrincipal: TPanel;
+
+    procedure opcionSalirClick(Sender: TObject);
 
   private
     { Private declarations }
@@ -31,7 +58,7 @@ implementation
 
 {$R *.fmx}
 
-procedure TForm1.MenuItem2Click(Sender: TObject);
+procedure TForm1.opcionSalirClick(Sender: TObject);
 begin
   close;
 end;
