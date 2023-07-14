18529b18b8510ffc85eafad7232be2baa627b884
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-07-13 22:46:23 -0400

    Falta hacer el analisis de tendencia

--- Analisis.pas
+++ Analisis.pas
@@ -5,18 +5,21 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMXTee.Engine,
-  FMXTee.Procs, FMXTee.Chart, FMX.Menus, FMXTee.Series;
+  FMXTee.Procs, FMXTee.Chart, FMX.Menus, FMXTee.Series, FMXTee.Chart.Functions;
 
 type
   TAnalisisTendenciario = class(TForm)
-    Chart1: TChart;
-    Series1: TFastLineSeries;
     MenuBar1: TMenuBar;
     opciones: TMenuItem;
     Diario: TMenuItem;
     Semanal: TMenuItem;
     Mensual: TMenuItem;
     Anual: TMenuItem;
+    graficoSenial: TChart;
+    Series2: TFastLineSeries;
+    TeeFunction1: TAverageTeeFunction;
+    Series1: TFastLineSeries;
+    TeeFunction2: TAverageTeeFunction;
   private
     { Private declarations }
   public
