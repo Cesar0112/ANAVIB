d9a04d4c1e250e6f7685a7954be60c9827c70fd0
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-07-13 20:32:24 -0400

    Ya funciona el manejo de rutas apenas
    Sale la exception RangeCheckError
    cuando el camino de una ruta esta en blanco

--- Rutas.pas
+++ Rutas.pas
@@ -4,12 +4,12 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
-  System.Variants,
+  System.Variants, System.StrUtils,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Rtti,
   FMX.Grid.Style, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Grid,
   FMX.StdCtrls, FMX.Edit, FMX.ComboEdit, FMX.Layouts, FMX.ListBox,
   ZAbstractConnection, ZConnection, UASUtilesDB, Data.DB,
-  ZAbstractRODataset, ZDataset;
+  ZAbstractRODataset, ZDataset, ZAbstractDataset;
 
 type
   TventanaRutas = class(TForm)
@@ -29,9 +29,16 @@ type
     combobox_maquina: TComboBox;
     btn_guardar: TButton;
     ZReadOnlyQuery1: TZReadOnlyQuery;
+    ZQuery1: TZQuery;
     procedure FormShow(Sender: TObject);
     procedure ComboEdit_etiquetaKeyDown(Sender: TObject; var Key: Word;
       var KeyChar: Char; Shift: TShiftState);
+    procedure ComboEdit_etiquetaChange(Sender: TObject);
+    procedure btn_agregar_horizontalClick(Sender: TObject);
+    procedure btn_agregar_verticalClick(Sender: TObject);
+    procedure btn_agregar_axialClick(Sender: TObject);
+    procedure btn_eliminar_todoClick(Sender: TObject);
+    procedure btn_eliminar_seleccionadoClick(Sender: TObject);
 
   private
     { Private declarations }
@@ -43,16 +50,157 @@ type
 
 procedure llenarcomboeditRutas();
 procedure llenarcomboMaquinas();
+procedure actualizarListaCamino(etiqueta: String);
 
 var
   ventanaRutas: TventanaRutas;
-  listado_rutas, maquinas: TStringList;
+  listado_camino, listado_rutas, maquinas: TStringList;
+  arrayCamino: TArray<String>;
 
 implementation
 
 {$R *.fmx}
 
 uses principal;
+
+procedure TventanaRutas.btn_agregar_axialClick(Sender: TObject);
+var
+  consulta: String;
+  elementoA: String;
+begin
+  elementoA := StringReplace(listado_camino.Text, #13#10, ',', [rfReplaceAll]) +
+    combobox_maquina.Items[combobox_maquina.ItemIndex] + 'A';
+  consulta := 'UPDATE rutas SET Camino="' + elementoA + '" WHERE Etiqueta="' +
+    ComboEdit_etiqueta.Text + '"';
+  ActualizarSQL(ZQuery1, consulta);
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+  try
+    ListBox_ruta.Items.AddStrings(listado_camino);
+    ListBox_ruta.ItemIndex := 0;
+  except
+    on E: Exception do
+  end;
+
+end;
+
+procedure TventanaRutas.btn_agregar_horizontalClick(Sender: TObject);
+var
+  consulta: String;
+  elementoH: String;
+begin
+  elementoH := StringReplace(listado_camino.Text, #13#10, ',', [rfReplaceAll]) +
+    combobox_maquina.Items[combobox_maquina.ItemIndex] + 'H';
+  consulta := 'UPDATE rutas SET Camino="' + elementoH + '" WHERE Etiqueta="' +
+    ComboEdit_etiqueta.Text + '"';
+  ActualizarSQL(ZQuery1, consulta);
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+
+  try
+    ListBox_ruta.Items.AddStrings(listado_camino);
+    ListBox_ruta.ItemIndex := 0;
+  except
+    on E: Exception do
+  end;
+
+end;
+
+procedure TventanaRutas.btn_agregar_verticalClick(Sender: TObject);
+var
+  consulta: String;
+  elementoV: String;
+begin
+  // obtengo todos los elementos en forma de string asi M1H,M2v....
+  elementoV := StringReplace(listado_camino.Text, #13#10, ',', [rfReplaceAll]) +
+    combobox_maquina.Items[combobox_maquina.ItemIndex] + 'V';
+  consulta := 'UPDATE rutas SET Camino="' + elementoV + '" WHERE Etiqueta="' +
+    ComboEdit_etiqueta.Text + '"';
+  ActualizarSQL(ZQuery1, consulta);
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+  try
+    ListBox_ruta.Items.AddStrings(listado_camino);
+    ListBox_ruta.ItemIndex := 0;
+  except
+    on E: Exception do
+  end;
+
+end;
+
+procedure TventanaRutas.btn_eliminar_seleccionadoClick(Sender: TObject);
+var
+  elementoSeleccionado: Integer;
+
+  consulta: String;
+
+  nuevoCamino: String;
+
+begin
+
+  if ListBox_ruta.ItemIndex <> -1 then
+  begin
+
+    elementoSeleccionado := ListBox_ruta.ItemIndex;
+    // elimino el elemnto seleccionado
+    listado_camino.Delete(elementoSeleccionado);
+
+    nuevoCamino := StringReplace(listado_camino.Text, #13#10, ',',
+      [rfReplaceAll]);
+    consulta := 'UPDATE rutas SET Camino="' + nuevoCamino + '" WHERE Etiqueta="'
+      + ComboEdit_etiqueta.Text + '"';
+    ActualizarSQL(ZQuery1, consulta);
+    ListBox_ruta.Clear;
+    actualizarListaCamino(ComboEdit_etiqueta.Text);
+    // agrega el camino
+    try
+      ListBox_ruta.Items.AddStrings(listado_camino);
+      ListBox_ruta.ItemIndex := 0;
+    except
+      on E: Exception do
+    end;
+
+  end;
+
+end;
+
+procedure TventanaRutas.btn_eliminar_todoClick(Sender: TObject);
+var
+  consulta: String;
+begin
+  consulta := 'UPDATE rutas SET Camino="" WHERE Etiqueta="' +
+    ComboEdit_etiqueta.Text + '"';
+  ActualizarSQL(ZQuery1, consulta);
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+
+  try
+    ListBox_ruta.Items.AddStrings(listado_camino);
+    ListBox_ruta.ItemIndex := 0;
+  except
+    on E: Exception do
+
+  end;
+
+end;
+
+procedure TventanaRutas.ComboEdit_etiquetaChange(Sender: TObject);
+begin
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+  try
+    ListBox_ruta.Items.AddStrings(listado_camino);
+    ListBox_ruta.ItemIndex := 0;
+  except
+    on E: Exception do
+  end;
+
+end;
 
 procedure TventanaRutas.ComboEdit_etiquetaKeyDown(Sender: TObject;
   var Key: Word; var KeyChar: Char; Shift: TShiftState);
@@ -75,12 +223,20 @@ begin
   llenarcomboMaquinas;
   combobox_maquina.Items.Assign(maquinas);
   combobox_maquina.ItemIndex := 0;
+
   llenarcomboeditRutas;
   ComboEdit_etiqueta.Items.Assign(listado_rutas);
   ComboEdit_etiqueta.ItemIndex := 0;
+
+  ListBox_ruta.Clear;
+  actualizarListaCamino(ComboEdit_etiqueta.Text);
+  // agrega el camino
+  ListBox_ruta.Items.Assign(listado_camino);
+  ListBox_ruta.ItemIndex := 0;
+
 end;
 
-procedure llenarcomboMaquinas();
+procedure llenarcomboMaquinas;
 var
   consulta: String;
 begin
@@ -128,6 +284,39 @@ begin
     end;
 
   end;
+end;
+
+procedure actualizarListaCamino(etiqueta: String);
+var
+  camino: String;
+  i: String;
+  arrayAux: TArray<String>;
+  tamArra: Integer;
+begin
+  listado_camino := TStringList.Create;
+
+  ConsultaSQL(ventanaRutas.ZReadOnlyQuery1,
+    'SELECT Camino FROM rutas WHERE Etiqueta="' + etiqueta + '"');
+  // si no esta vacia la consulta
+  if not ventanaRutas.ZReadOnlyQuery1.IsEmpty then
+  begin
+    camino := trim(ventanaRutas.ZReadOnlyQuery1.FieldByName('Camino').AsString);
+
+    // elimina las comas al principio y al final si las tiene
+    if camino[camino.Length] = ',' then
+      Delete(camino, camino.Length, 1);
+    if camino[1] = ',' then
+      Delete(camino, 1, 1);
+
+    arrayAux := SplitString(camino, ','); // array divido por comas;
+
+    for i in arrayAux do
+    begin
+      listado_camino.Add(i);
+    end;
+
+  end;
+
 end;
 
 end.
