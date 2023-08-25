39aa93e9118c04f0395e9148ec1f26c0eae26003
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-08-25 15:17:08 -0400

    Archivos movidos a SRC/Visual
    Por alguna razon no encuentro Config.cfg

--- ANAVIB.dpr
+++ ANAVIB.dpr
@@ -3,16 +3,16 @@ program ANAVIB;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  Login in 'Login.pas' {formLogin},
-  principal in 'principal.pas' {formPrincipal},
-  Configuracion in 'Configuracion.pas' {formConfiguracion},
-  Analisis in 'Analisis.pas' {AnalisisTendenciario},
-  Rutas in 'Rutas.pas' {ventanaRutas},
-  Usuarios in 'Usuarios.pas' {formUsuarios},
-  Seguridad in 'Seguridad.pas',
-  Seleccion in 'Seleccion.pas' {ventanaSeleccion},
- // calculoFFT in 'calculoFFT.pas',
-  fftCalculo in 'fftCalculo.pas';
+  Login in 'Src\Visual\Login.pas' {formLogin},
+  principal in 'Src\Visual\principal.pas' {formPrincipal},
+  Rutas in 'Src\Visual\Rutas.pas' {ventanaRutas},
+  Usuarios in 'Src\Visual\Usuarios.pas' {formUsuarios},
+  CambioDeUbicacion in 'Src\Visual\CambioDeUbicacion.pas' {Form1},
+  Configuracion in 'Src\Visual\Configuracion.pas' {formConfiguracion},
+  Analisis in 'Src\Visual\Analisis.pas' {AnalisisTendenciario},
+  Seleccion in 'Src\Visual\Seleccion.pas' {ventanaSeleccion},
+  fftCalculo in 'Src\Logic\fftCalculo.pas',
+  Seguridad in 'Src\Logic\Seguridad.pas';
 
 {$R *.res}
 
@@ -20,10 +20,11 @@ begin
   Application.Initialize;
   Application.CreateForm(TformLogin, formLogin);
   Application.CreateForm(TformPrincipal, formPrincipal);
-  Application.CreateForm(TformConfiguracion, formConfiguracion);
-  Application.CreateForm(TAnalisisTendenciario, AnalisisTendenciario);
   Application.CreateForm(TventanaRutas, ventanaRutas);
   Application.CreateForm(TformUsuarios, formUsuarios);
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TformConfiguracion, formConfiguracion);
+  Application.CreateForm(TAnalisisTendenciario, AnalisisTendenciario);
   Application.CreateForm(TventanaSeleccion, ventanaSeleccion);
   Application.Run;
 end.
